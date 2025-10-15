f1bd65ab3708f14c6a0722d83d8645673b7901df
Яба Сист <52179226+RomaIsakov@users.noreply.github.com>
2025-10-15 13:44:50 +0300

    Add files via upload

--- solutions.c
+++ solutions.c
@@ -1,4 +1,4 @@
-#include "lab1.h"
+#include "lab2.h"
 #include <math.h>
 #include <stdio.h>
 
@@ -9,10 +9,10 @@ int is_even(int n) {
 
 // Задача 2 - Максимум из трёх чисел
 int max_of_three(int a, int b, int c) {
-    int max = a
-    if (b > max) max = b
-    if (c > max) max = c
-    return max
+    int max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    return max;
 }
 
 // Задача 3 - Знак числа
@@ -41,7 +41,7 @@ const char* get_grade(int score) {
     else if (score >= 75 && score <= 89) return "хорошо";
     else if (score >= 60 && score <= 74) return "удовлетворительно";
     else if (score >= 0 && score <= 59) return "неудовлетворительно";
-    else return "некорректно"
+    else return "некорректно";
 }
 
 // Задача 7 - Евклидово расстояние
@@ -56,14 +56,14 @@ int manhattan_distance(int x1, int y1, int x2, int y2) {
 
 // Вспомогательная функция для проверки простоты (можно писать сразу в задаче 9, но это плохой тон)
 int is_prime(int n) {
-    if (n <= 1) return false;
-    if (n == 2) return true;
-    if (n % 2 == 0) return false;
+    if (n <= 1) return 0;
+    if (n == 2) return 1;
+    if (n % 2 == 0) return 0;
     
     for (int i = 3; i * i <= n; i += 2) {
-        if (n % i == 0) return false;
+        if (n % i == 0) return 0;
     }
-    return true;
+    return 1;
 }
 
 // Задача 9 - Среднее арифметическое простых чисел в интервале
@@ -72,14 +72,14 @@ double average_primes_in_interval(int a, int b) {
     int count = 0;
     
     for (int i = a; i <= b; i+=1) {
-        if (isPrime(i)) {
+        if (is_prime(i)) {
             sum += i;
             count+=1;
         }
     }
     
     if (count == 0) return 0;
-    else return sum / count
+    else return sum / count;
 }
 
 // Задача 10 - Кратные числа в интервале
@@ -100,8 +100,39 @@ int is_leap_year(int year) {
 }
 
 // Задача 12 - Сезон по номеру месяца
-const char* season_by_month(int month) {
-        switch(day) {
+const char* season_by_month(int month)
+{
+    switch(month) {
+        case 12:
+        case 1:
+        case 2:
+            return("Зима\n");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            return("Весна\n");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            return("Лето\n");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            return("Осень\n");
+            break;
+        default:
+            return("Некорректно\n");
+    }
+    
+    return 0;
+}
+
+// Задача 13 - День недели
+const char* day_of_week(int day_num) {
+        switch(day_num) {
         case 1:
             return "понедельник";
             break;
@@ -127,14 +158,20 @@ const char* season_by_month(int month) {
             return"некорректно";
     }
 }
-// Задача 13 - День недели
-const char* day_of_week(int day_num) {
-    // TODO: реализовать
-}
 
 // Задача 14 - Подсчёт цифр в числе
-int count_digits(int n) {
-    // TODO: реализовать
+int count_digits(int n)
+{
+    if (n<0){
+        n = -1*n;
+    }
+    int count = 0;
+    do {
+        count++;
+        n/=10;
+    }
+    while (n>0);
+    return count;
 }
 
 // Задача 15 - Реверс числа
